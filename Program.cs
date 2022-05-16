d3397418a5995b44ed41e4d7f3918e2a0b7f437e
michalomegalul <92925864+michalomegalul@users.noreply.github.com>
2022-05-16 13:23:33 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿new TestApp().Run();
+class TestApp
+{
+    public void Run()
+    {
+        var data = GetIUtemsFromData(); var sum = 0;
+        foreach (var item in data)
+        {
+            sum += Convert.ToInt32(item.Quantity) * Convert.ToInt32(item.Price);
+        }
+        File.WriteAllText("./output.txt", $"Datum:{DateTime.Now}; Celkova castka:{sum}");
+        Console.WriteLine($"Datum:{DateTime.Now}; Celkova castka:{sum}");
+    }
+    public List<Items> GetIUtemsFromData()
+    {
+        var data = File.ReadAllText(@"./inventory.txt");
+        var list = new List<Items>();
+
+        var parts = data.Split('#');
+        foreach (var dota in parts)
+        {
+            var splititem = dota.Split('-');
+            var Items = new Items
+            {
+                Price = splititem[4],
+                Quantity = splititem[5],
+            };
+            list.Add(Items);
+        }
+        return list;
+    }
+}
+public class Items
+{
+    public string Price { get; set; } = "";
+    public string Quantity { get; set; } = "";
+}
